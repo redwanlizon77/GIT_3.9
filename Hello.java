1e75215fd435e8714de41ae665d68b8d3bbb82ae
Redwan Bin Wahab <redwanlizon77@gmail.com>
2025-02-16 11:27:44 +0600

    Added a method called greeting in hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void main(String[] args) {
+        System.out.println("vs code is awesome");
+    }
+
+    public static void greeting (String name){
+        System.out.println("hello" +name);
+    }
+}
