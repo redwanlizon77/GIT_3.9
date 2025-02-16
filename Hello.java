55583045fc651c7cf2fbaab3fadbb8811c5b21a2
Redwan Bin Wahab <redwanlizon77@gmail.com>
2025-02-16 11:41:15 +0600

    Changed greeting method

--- Hello.java
+++ Hello.java
@@ -1,9 +1,11 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("vs code is awesome");
+
+        greeting("Git");
     }
 
     public static void greeting (String name){
-        System.out.println("hello" +name);
+        System.out.println("Bye bye" +name);
     }
 }
